96abe0f1fd389b32bda1a3f52231ef79230690b2
Armaan Singh Sidhu <armaansidhu686@gmail.com>
2025-03-31 21:12:54 +1100

    this is the first code for the scraper, includes requesting the url

--- scraper.py
+++ scraper.py
@@ -1,1 +1,12 @@
-print("hello world")
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://en.wikipedia.org/wiki/Real_analysis'
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    print("Successfully fetched the webpage!")
+else:
+    print(f"Failed to retrieve the webpage. Status code: {response.status_code}")
+
